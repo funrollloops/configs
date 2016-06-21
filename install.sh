8bf5630109e4019a7c9e55188183bd7af80f73a7
Sagar Mittal <sagar@sagarmittal.com>
2016-06-20 22:45:34 -0700

    Install symlinks for nvim

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
+#!/bin/bash
+
 files=$(find .  -maxdepth 1 -name '.*' -type f  -not -name '*.swp' |
         xargs -n1 readlink -f)
 
-for file in $files; do
-  dest="$HOME/$(basename $file)"
-  echo -ne "installing $file..."
+function link {
+  local src=$1
+  local dest=$2
+  echo -ne "installing $src..."
   if [ ! -e "$dest" ]; then
-    ln -s "$file" "$dest"
+    ln -s "$src" "$dest"
     echo ok
-  elif [ "$(readlink -f "$dest")" != "$file" ]; then
+  elif [ "$(readlink -f "$dest")" != "$src" ]; then
     echo "file in the way, remove"
     ls -l "$dest"
   else
     echo already there
   fi
+}
+
+for file in $files; do
+  link "$file" "$HOME/$(basename $file)"
 done
+
+mkdir -p ~/.config/nvim ~/.vim/{autoload,bundle}
+link ~/.vim/autoload ~/.config/nvim/autoload
+link ~/.vim/bundle ~/.config/nvim/bundle
+link $(readlink -f ./.vimrc) ~/.config/nvim/init.vim
 
 echo done
