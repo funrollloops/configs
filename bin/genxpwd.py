f0a2c1079cf64911ea7e02566fc42181af9ba66c
Sagar Mittal <sagar@sagarmittal.com>
2018-06-16 23:23:03 -0700

    Add genxpwd.py

---
+++ bin/genxpwd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from random import choice
+from math import log
+
+words = [x.strip() for x in open('/usr/share/dict/words').readlines()]
+words = list(set(w for w in words if 3 < len(w) < 9 and w.isalpha() and w.islower()))
+
+print('entropy=%s bits' % (3 * log(len(words))/log(2)))
+
+print(' '.join(choice(words) for _ in range(4)))
+
