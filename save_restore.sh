93f6b15d86e19c8f8f42471bb9553b172abc44eb
Sagar Mittal <sagar@sagarmittal.com>
2022-04-13 21:58:21 -0700

    Add script to save/restore home directory

---
+++ save_restore.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eufx -o pipefail
+
+function save {
+  local OUTPUT=${1:-/tmp/home.tar.zst.gpg}
+  (
+    cd ~ &&
+    tar --exclude .cache --exclude .local/share/Steam \
+      --exclude .config/google-chrome \
+      --exclude Downloads/ \
+      -cf - .
+  ) |
+    zstd -10 -T0 |
+    time gpg -o "$OUTPUT" -c
+  ls -lh "$OUTPUT"
+}
+
+function restore {
+  if [ ! -e "$1" ]; then
+    echo "$1" does not exist
+    return
+  fi
+  # Replace with xpf to extract.
+  gpg -d "$1" | unzstd -T0 | time tar -tpf -
+}
+
+
+CMD=$1
+shift
+"$CMD" "$@"
